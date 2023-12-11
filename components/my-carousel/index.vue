916132ab142a0db74b92aafca37ddecc7d231f12
Milk-Ice <mathila1218@gmail.com>
2023-12-11 21:45:40 +0800

    feat(carousel): 03_轮播图动态数据的展示和自定义指示器

--- components/my-carousel/index.vue
+++ components/my-carousel/index.vue
@@ -1,25 +1,69 @@
 <template>
   <div class="carousel">
-    <el-carousel>
-      <el-carousel-item v-for="item in 4" :key="item">
-        <img
-          src="https://www.oppo.com/content/dam/oppo/product-asset-library/find/find-n3-series/cn/find-n3/v1/assets/images-kv-kv.jpg?v=1698134093000"
-          alt=""
-        />
+    <el-carousel
+      height="480px"
+      indicator-position="none"
+      @change="handleCarouselChange"
+    >
+      <el-carousel-item v-for="item in banners" :key="item.id">
+        <img class="pic-str" :src="item.picStr" alt="" />
       </el-carousel-item>
     </el-carousel>
+    <ul class="dots">
+      <li
+        v-for="(item, index) in banners"
+        :key="item.id"
+        :class="['dot', currentIndex === index ? 'active' : '']"
+      ></li>
+    </ul>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ElCarousel } from 'element-plus'
 import { ElCarouselItem } from 'element-plus'
-// interface IProps {
-//   title?: string
-// }
-// const props = withDefaults(defineProps<IProps>(), {
-//   title: ''
-// })
+import { IBanners } from '~~/types/home'
+interface IProps {
+  banners?: IBanners[]
+}
+withDefaults(defineProps<IProps>(), {
+  banners: () => []
+})
+const currentIndex = ref<number>(0)
+function handleCarouselChange(index: number) {
+  currentIndex.value = index
+  // console.log(currentIndex.value)
+}
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.carousel {
+  position: relative;
+}
+.dots {
+  height: 40px;
+  width: 100%;
+  position: absolute;
+  bottom: 0px;
+  left: 0;
+  text-align: center;
+  .dot {
+    display: inline-block;
+    width: 10px;
+    height: 10px;
+    box-sizing: border-box;
+    border-radius: 10px;
+    margin-right: 10px;
+    background-color: rgb(255, 255, 255);
+    opacity: 0.8;
+  }
+  .active {
+    background-color: transparent;
+    border: 2px solid #fff;
+    width: 12px;
+    height: 12px;
+    position: relative;
+    top: 1px;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,20 @@
 <template>
   <div class="home wrapper">
-    <my-carousel></my-carousel>
+    <my-carousel :banners="banners"></my-carousel>
   </div>
 </template>
 
 <script setup lang="ts">
-interface IProps {
-  title: string
-}
-withDefaults(defineProps<IProps>(), {
-  title: ''
-})
+// interface IProps {
+//   title: string
+// }
+// withDefaults(defineProps<IProps>(), {
+//   title: ''
+// })
+import useHomeStore from '@/store/index'
+import { storeToRefs } from 'pinia'
+const homeStore = useHomeStore()
+const { banners } = storeToRefs(homeStore)
 </script>
 
 <style lang="scss">
